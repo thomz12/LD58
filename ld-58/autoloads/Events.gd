0724cb83795fcb15e72367588e706061255a65ba
Mathijs Koning <mk@mathijskoning.nl>
2025-10-04 16:41:03 +0200

    Ignore warnings

--- ld-58/autoloads/Events.gd
+++ ld-58/autoloads/Events.gd
@@ -1,7 +1,10 @@
 extends Node
 
+@warning_ignore("unused_signal")
 signal add_camera_target(target: Node2D)
+@warning_ignore("unused_signal")
 signal fish_caught(fish: Fish)
-
+@warning_ignore("unused_signal")
 signal float_cast()
+@warning_ignore("unused_signal")
 signal float_return()
