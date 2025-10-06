70fc4a67246190f8044e2770db7237c0dc0cee47
Mathijs Koning <mk@mathijskoning.nl>
2025-10-06 18:28:12 +0200

    Fix issue by doing it in a completely different way

--- ld-58/scripts/altar_image.gd
+++ ld-58/scripts/altar_image.gd
@@ -1,9 +1,11 @@
 extends Sprite2D
 
+@onready var rich_text_label: RichTextLabel = $RichTextLabel
 
 func _on_area_2d_mouse_entered() -> void:
-	Events.ui_dislay_fish_stat.emit(name, self)
+	rich_text_label.show()
+	rich_text_label.text = "[bgcolor=BLACK] caught: %s" % Progress.get_catch_count_by_identifier(name)
 
 
 func _on_area_2d_mouse_exited() -> void:
-	Events.ui_stop_display_fish_stat.emit()
+	rich_text_label.hide()
--- ld-58/scripts/ui/hud.gd
+++ ld-58/scripts/ui/hud.gd
@@ -2,7 +2,6 @@ extends Control
 class_name HUD
 
 @onready var label_start_info: RichTextLabel = %LabelStartInfo
-@onready var label_fish_info: Label = %LabelFishInfo
 
 var notification_won_scene := load("res://scenes/ui/notifications/notification_game_won.tscn")
 var notification_fish_scene := load("res://scenes/ui/notifications/notification_new_fish_type.tscn")
@@ -13,16 +12,6 @@ var game_over := false
 func _ready() -> void:
 	Events.notification_new_fish_type.connect(_on_new_fish_type_caught)
 	Events.game_won.connect(_on_game_won)
-	Events.ui_dislay_fish_stat.connect(
-		func(fish: String, node: Node2D):
-			label_fish_info.show()
-			label_fish_info.text = "Caught: %s" % Progress.get_catch_count_by_identifier(fish)
-			label_fish_info.global_position = node.global_position + Vector2(8, -8)
-	)
-	Events.ui_stop_display_fish_stat.connect(
-		func():
-			label_fish_info.text = ""
-	)
 
 func _process(delta: float) -> void:
 	if not game_over: game_time += delta
